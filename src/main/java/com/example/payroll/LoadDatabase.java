dd5935082dfe7cafaf8d4a91a6171f135b4845fc
Joshua Farayola <farayolajoshua@gmail.com>
2023-01-29 13:50:48 +0100

    Updated database init

--- src/main/java/com/example/payroll/LoadDatabase.java
+++ src/main/java/com/example/payroll/LoadDatabase.java
@@ -2,6 +2,9 @@ package com.example.payroll;
 
 import com.example.payroll.employee.Employee;
 import com.example.payroll.employee.EmployeeRepository;
+import com.example.payroll.order.Order;
+import com.example.payroll.order.OrderRepository;
+import com.example.payroll.order.OrderStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -11,10 +14,17 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 public class LoadDatabase {
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
+    CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository) {
         return args -> {
-            log.info("Preloading {}", repository.save(new Employee("Bilbo", "Bagginss", "burglar")));
-            log.info("Preloading {}", repository.save(new Employee("Frodo", "Baggins", "thief")));
+            employeeRepository.save(new Employee("Bilbo", "Bagginss", "burglar"));
+            employeeRepository.save(new Employee("Frodo", "Baggins", "thief"));
+
+            employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+
+            orderRepository.save(new Order("MacBook Pro", OrderStatus.COMPLETED));
+            orderRepository.save(new Order("iPhone", OrderStatus.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> log.info("Preloaded " + order));
         };
     }
 }
